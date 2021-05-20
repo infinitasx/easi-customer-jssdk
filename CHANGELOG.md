9a8c56997be8d7031e0c2e7d4007c315b68562a5
Chris Chen <me.chenying@gmail.com>
2021-05-20 15:16:20 +0800

    feat: 1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@
 
 本文件格式基于 [Keep a Changelog](https://keepachangelog.com/zh-CN/1.0.0/) ，并且遵循 [Semantic Versioning](https://semver.org/spec/v2.0.0.html) 。
 
+## [1.2.0] - 2021-05-20
+### Added
+- 发布时生成 README.html
+
 ## [1.1.1] - 2021-05-20
 ### Added
 - 接入 DevOps
--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
+import copy from 'rollup-plugin-copy';
+import MarkdownIt from 'markdown-it';
 
 export default {
   input: "src/index.js",
@@ -41,5 +43,23 @@ export default {
     babel({
       exclude: "node_modules/**",
     }),
+    copy({
+      targets: [
+        {
+          src: 'README.md',
+          dest: 'dist',
+          rename: 'README.html',
+          transform: (contents) => {
+            return [
+              '<meta charset="utf-8">',
+              '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css" integrity="sha512-Oy18vBnbSJkXTndr2n6lDMO5NN31UljR8e/ICzVPrGpSud4Gkckb8yUpqhKuUNoE+o9gAb4O/rAxxw1ojyUVzg==" crossorigin="anonymous" referrerpolicy="no-referrer" />',
+              '<div class="markdown-body">',
+              new MarkdownIt().render(contents.toString('utf-8')),
+              '</div>'
+            ].join('\n')
+          }
+        }
+      ]
+    })
   ],
 };
