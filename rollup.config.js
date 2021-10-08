34ad372f7c1689ce33c17db5849726fe503351c4
huyanhai <810839700@qq.com>
2021-10-08 17:27:48 +0800

    chore: 构建源码并生成docs

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,7 @@ import { babel } from '@rollup/plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import copy from 'rollup-plugin-copy';
 import esbuild from 'rollup-plugin-esbuild';
-import MarkdownIt from 'markdown-it';
 import ts from 'rollup-plugin-typescript2';
 
 export default [
@@ -52,24 +50,6 @@ export default [
         babelHelpers: 'bundled',
       }),
       terser(),
-      copy({
-        targets: [
-          {
-            src: 'README.md',
-            dest: 'dist',
-            rename: 'README.html',
-            transform: contents => {
-              return [
-                '<meta charset="utf-8">',
-                '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css" integrity="sha512-Oy18vBnbSJkXTndr2n6lDMO5NN31UljR8e/ICzVPrGpSud4Gkckb8yUpqhKuUNoE+o9gAb4O/rAxxw1ojyUVzg==" crossorigin="anonymous" referrerpolicy="no-referrer" />',
-                '<div class="markdown-body">',
-                new MarkdownIt().render(contents.toString('utf-8')),
-                '</div>',
-              ].join('\n');
-            },
-          },
-        ],
-      }),
     ],
   },
   {
@@ -92,24 +72,6 @@ export default [
         babelHelpers: 'bundled',
       }),
       terser(),
-      copy({
-        targets: [
-          {
-            src: 'README.md',
-            dest: 'dist',
-            rename: 'README.html',
-            transform: contents => {
-              return [
-                '<meta charset="utf-8">',
-                '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css" integrity="sha512-Oy18vBnbSJkXTndr2n6lDMO5NN31UljR8e/ICzVPrGpSud4Gkckb8yUpqhKuUNoE+o9gAb4O/rAxxw1ojyUVzg==" crossorigin="anonymous" referrerpolicy="no-referrer" />',
-                '<div class="markdown-body">',
-                new MarkdownIt().render(contents.toString('utf-8')),
-                '</div>',
-              ].join('\n');
-            },
-          },
-        ],
-      }),
     ],
   },
 ];
