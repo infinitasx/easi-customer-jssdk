ff8e001d8dd25b0484d8701b59d6e5a7714b9769
huyanhai <810839700@qq.com>
2021-10-11 14:39:28 +0800

    chore: 修改logo图片

---
+++ dist/docs/assets/zh_api_chooseimage copy.md.44e8ee3c.lean.js
@@ -0,0 +1,1 @@
+import{_ as n,c as t,a,g as s}from"./app.e4147315.js";const p='{"title":"easi.chooseImage","description":"","frontmatter":{},"headers":[{"level":2,"title":"参数","slug":"参数"}],"relativePath":"zh/api/chooseimage copy.md","lastUpdated":1633686787360}',o={},e=[s('',8)];var c=n(o,[["render",function(n,s,p,o,c,u){return a(),t("div",null,e)}]]);export{p as __pageData,c as default};
